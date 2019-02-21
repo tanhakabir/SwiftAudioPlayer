58d1695cba24a41aab599201f29f2c7b0ede1200
Tanha <tanhakabir.ca@gmail.com>
2019-02-20 16:20:46 -0800

    add basic UI elements

--- Example/SwiftAudioPlayer/ViewController.swift
+++ Example/SwiftAudioPlayer/ViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 import SwiftAudioPlayer
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bufferProgress: UIProgressView!
+    @IBOutlet weak var scrubberSlider: UISlider!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var skipBackwardButton: UIButton!
+    @IBOutlet weak var skipForwardButton: UIButton!
+    
+    @IBOutlet weak var audioSelector: UISegmentedControl!
+    @IBOutlet weak var streamButton: UIButton!
+    @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var rateSlider: UISlider!
+    
+    @IBOutlet weak var rateLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        adjustSpeed()
+        
         var foo = SALockScreenInfo(title: "", artist: "", artwork: UIImage(), releaseDate: 12)
-        SAPlayer.shared.rate = 1.0
         SAPlayer.shared.mediaInfo = foo
         SAPlayer.shared.initializeAudio(withRemoteUrl: URL(string: "https://traffic.megaphone.fm/TTH7630150098.mp3")!)
 
@@ -26,6 +40,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func scrubberSeeked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func rateChanged(_ sender: Any) {
+        adjustSpeed()
+    }
+    
+    private func adjustSpeed() {
+        let speed = rateSlider.value
+        rateLabel.text = "rate: \(speed)x"
+        SAPlayer.shared.rate = Double(speed)
+    }
+    
 }
 

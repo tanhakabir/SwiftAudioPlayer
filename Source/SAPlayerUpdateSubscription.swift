2c50502b28aba7aaa840b9cda9d2e11ffba37408
Tanha <tanhakabir.ca@gmail.com>
2019-12-18 16:19:37 -0800

    update documentation for live streaming audio

--- Source/SAPlayerUpdateSubscription.swift
+++ Source/SAPlayerUpdateSubscription.swift
@@ -66,11 +66,15 @@ extension SAPlayer {
         
         /**
          Updates to changes in the duration of the current initialized audio. Especially helpful for audio that is being streamed and can change with more data.
+         
+         - Note: If you are streaming from a source that does not have an expected size at the beginning of a stream, such as live streams, duration will be constantly updating to best known value at the time (which is the seconds buffered currently and not necessarily the actual total duration of audio).
          */
         public struct Duration {
             
             /**
              Subscribe to updates to changes in duration of the current audio initialized.
+             
+             - Note: If you are streaming from a source that does not have an expected size at the beginning of a stream, such as live streams, duration will be constantly updating to best known value at the time (which is the seconds buffered currently and not necessarily the actual total duration of audio).
              
              - Note: It's recommended to have a weak reference to a class that uses this function
              
@@ -136,6 +140,8 @@ extension SAPlayer {
             /**
              Subscribe to updates to changes in the progress of downloading audio for streaming. Information about range of audio available and if the audio is playable. Look at SAAudioAvailabilityRange for more information. For progress of downloading audio that saves to the phone for playback later, look at AudioDownloading instead.
              
+             - Note: For live streams that don't have an expected audio length from the beginning of the stream; the duration is constantly changing and equal to the total seconds buffered from the SAAudioAvailabilityRange.
+             
              - Note: It's recommended to have a weak reference to a class that uses this function
              
              - Parameter closure: The closure that will receive the updates of the changes in duration.
@@ -172,7 +178,7 @@ extension SAPlayer {
              
              - Parameter closure: The closure that will receive the updates of the changes in duration.
              - Parameter url: The corresponding remote URL for the updated download progress.
-             - Parameter progress: Value from 0.0 to 1.0 indicating progress of download. This is unreliable for live streams that don't have an expected audio length from the beginning of the stream; in that case it will always be 1.0.
+             - Parameter progress: Value from 0.0 to 1.0 indicating progress of download.
              - Returns: the id for the subscription in the case you would like to unsubscribe to updates for the closure.
              */
             public static func subscribe(_ closure: @escaping (_ url: URL, _ progress: Double) -> ()) -> UInt {
